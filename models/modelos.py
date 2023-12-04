cdfb41effb4f824443f217985af88f2bb4efdf6d
devmarcs <devmarcelo.gus@gmail.com>
2023-12-04 10:35:25 -0300

    retirando a opção de imagem

--- models/modelos.py
+++ models/modelos.py
@@ -18,7 +18,7 @@ class Jogos(db.Model):
 
 class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(8), primary_key=True)
+    username = db.Column(db.String(8), nullable=False)
     nome = db.Column(db.String(20), nullable=False)
     senha = db.Column(db.String(100), nullable=False)
 
